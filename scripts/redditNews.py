01fb0cf95a8c88b9f346ef7329113049b42a5160
Pranal Madria <pranalmadria@gmail.com>
2020-10-17 04:15:03 -0500

    time for sleep and reddit iterates everything grabs all of the text from news articles

--- scripts/redditNews.py
+++ scripts/redditNews.py
@@ -1,15 +1,18 @@
 import praw
 import pandas as pd
 from datetime import datetime, timezone
+
+#grab url and CSV
     
 reddit = praw.Reddit(client_id="VtubGoPRN3oLtg",
                      client_secret="XMgArUXTZz40OaWLKM046twdrJk",
                      password="ppF7egPkBAqT4V5",
                      user_agent="ranker by u/newrankbot",
                      username="NewsRankBot")
-# file = open("redditArticles.txt", "w")
-data = pd.DataFrame(columns = ['URL', 'Date'])
+file = open("redditArticles.txt", "w")
+data = pd.DataFrame(columns = ['URL', 'Reddit Date'])
 for submission in reddit.subreddit('canada').new(limit=100):
-    data = data.append({'URL' : submission.url, 'Date' : datetime.fromtimestamp(submission.created_utc).strftime('%Y-%m-%d %H:%M:%S')[0:10]}, ignore_index=True)
-# file.close()
-    
+    data = data.append({'URL' : submission.url, 'Reddit Date' : datetime.fromtimestamp(submission.created_utc).strftime('%Y-%m-%d %H:%M:%S')[0:10]}, ignore_index=True)
+    file.write(submission.url + '\n')
+pd.DataFrame(data).to_csv("redditDate.csv", index = False)
+file.close()
