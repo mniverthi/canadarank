3ca84f4b70fccf866bfe6f8613b44bdbae05b103
Pranal Madria <pranalmadria@gmail.com>
2020-10-17 03:17:48 -0500

    Text from every file

---
+++ scripts/getText.py
@@ -0,0 +1,32 @@
+from newspaper import Article
+import traceback
+import sys
+import numpy as np
+import praw
+
+
+
+
+# Take URL convert to text
+file = open("../scripts/redditArticles.txt", "r")
+for index, url in enumerate(file.readlines()):  
+  try:
+    article = Article(url.strip())
+    article.download()
+    article.parse()
+    if (len(article.text) == 0):  
+      print(url)
+    else:
+      path = "./text/"+str(index)+".txt"
+      writeFile = open(path, "a+")
+      writeFile.write(article.text)
+      writeFile.close()
+      
+  except:
+    print("Errored on: ", url)
+    traceback.print_exception(*sys.exc_info()) 
+file.close()
+
+
+
+    
