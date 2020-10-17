3fc7be9cb1ce5658bcf2b9731dd3df29aeed62f3
davidmkwon <surval2001@gmail.com>
2020-10-17 17:20:08 -0400

    working client?

--- dft/client.py
+++ dft/client.py
@@ -1,5 +1,6 @@
 from backend import api
 import os
+import pandas as pd
 
 def find_prob(model, text):
     output, all_count = model.check_probabilities(text)
@@ -9,15 +10,13 @@ def find_prob(model, text):
         if prob >= 0.1:
             count += 1
 
-    print(count, all_count)
     return count / all_count
 
 def get_text_list():
     texts = []
     base_dir = "../scripts/fulltext/"
-    for i in range(100):
+    for i in range(93):
         file_name = "{}{}.txt".format(base_dir, i)
-        print(file_name)
         file = open(file_name, "r")
         text = file.read()
         texts.append(text)
@@ -28,6 +27,13 @@ def get_text_list():
 if __name__ == '__main__':
     lm = api.LM()
     texts = get_text_list()
+    probs = []
     for text in texts:
         prob = find_prob(lm, text)
         print(prob)
+        probs.append(prob)
+
+    print(len(probs))
+    df = pd.read_csv('../scripts/data/news.csv')
+    df['Fake Scores'] = probs
+    pd.DataFrame(df).to_csv('../scripts/data/news1.csv', index=False)
